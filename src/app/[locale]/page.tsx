cb9498f3310e5ba5163ddec11a73c94880ceb80c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-03 22:24:11 +0300

    added basic styles and variables

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <p className="font-actay text-[64px] font-bold">TRAFFIC GAME</p>
-      <p className="font-guano-apes">Перевірка</p>
-    </>
-  );
+  return <></>;
 }
