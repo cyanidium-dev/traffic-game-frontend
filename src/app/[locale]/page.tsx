23233a7a9d71c9a108efefdab150fd45d61b70f5
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-05 15:49:59 +0300

    added benefits texts

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/home/about/About";
+import Benefits from "@/components/home/benefits/Benefits";
 import Blog from "@/components/home/blog/Blog";
 import Hero from "@/components/home/hero/Hero";
 import Offers from "@/components/home/offers/Offers";
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <Benefits />
       <Partners />
       <About />
       <Vacancies />
---
+++ src/components/home/benefits/Benefits.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/shared/container/Container";
+
+export default function Benefits() {
+  return (
+    <section className="pt-25 pb-24 md:py-[47px]">
+      <Container></Container>
+    </section>
+  );
+}
