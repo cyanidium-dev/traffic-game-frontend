fd359eb8802e6f7cd08cc1df57fdc5b0d987b619
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-06 16:42:15 +0300

    added offers section

--- src/components/home/offers/Offers.tsx
+++ src/components/home/offers/Offers.tsx
@@ -1,3 +1,34 @@
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
+import MainButton from "@/components/shared/buttons/MainButton";
+import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/sectionTitle/SectionTitle";
+import { useTranslations } from "next-intl";
+import OffersList from "./OffersList";
+
 export default function Offers() {
-  return <section>Offers</section>;
+  const t = useTranslations("homePage.offers");
+
+  return (
+    <section className="pt-[202px] pb-[127px] xl:pt-[642px] xl:pb-[225px]">
+      <Container>
+        <div className="max-w-[214px] md:max-w-[300px] xl:max-w-[442px] mx-auto md:ml-auto md:mr-0">
+          <SectionTitle className="max-w-[214px] xl:max-w-[442px] mb-150 md:mb-9 text-[48px] xl:text-[94px] text-center md:text-left">
+            {t("title")}
+          </SectionTitle>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({ scale: 0.9, delay: 2.4 })}
+            className="hidden md:flex max-w-[214px] md:max-w-[300px] xl:max-w-[418px]"
+          >
+            <MainButton>{t("button")}</MainButton>
+          </motion.div>
+        </div>
+        <OffersList />
+      </Container>
+    </section>
+  );
 }
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -16,10 +16,10 @@ export default function Header() {
   });
 
   return (
-    <header className="fixed top-0 left-0 z-50 py-9 xl:py-10 w-dvw">
+    <header className="fixed top-0 left-0 z-50 py-6 xl:py-10 w-dvw">
       <Container className="relative flex items-center justify-between">
         <div
-          className={`absolute -top-6 left-0 w-full h-16 xl:h-17 rounded-full z-[-20] transition duration-500 ease-in-out ${
+          className={`absolute -top-3 md:-top-6 left-0 w-full h-16 xl:h-17 rounded-full z-[-20] transition duration-500 ease-in-out ${
             isScrolled
               ? "bg-black/30 backdrop-blur-md shadow-lg"
               : "bg-transparent"
