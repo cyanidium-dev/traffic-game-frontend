7ba64ba874872d3b01e6de4a952ed86bb870edb6
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-16 18:02:51 +0300

    updated marquee styles

--- src/components/home/hero/MarqueeLine.tsx
+++ src/components/home/hero/MarqueeLine.tsx
@@ -9,7 +9,7 @@ export default function MarqueeLine() {
     <Marquee
       autoFill={true}
       speed={100}
-      className="min-w-[110vw] md:min-w-[40vw] h-[27px] xl:h-[50px] rotate-[7.47deg] md:rotate-[17.04deg] bg-[linear-gradient(90deg,_#F80498_30.2%,_#920259_59.69%)] text-black"
+      className="flex items-center min-w-[110vw] md:min-w-[40vw] rotate-[7.47deg] md:rotate-[17.04deg] bg-[linear-gradient(90deg,_#F80498_30.2%,_#920259_59.69%)] text-black"
     >
       <span className="inline-block mt-1 xl:mt-2 mx-[3px] font-actay text-[19px] xl:text-[36px] font-bold uppercase">
         {t("betting")}
