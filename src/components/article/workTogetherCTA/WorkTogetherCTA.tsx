de94fccad1b80ba5dfe932d5b8f484a6ff679eeb
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-17 16:12:01 +0300

    updated section title

--- src/components/article/workTogetherCTA/WorkTogetherCTA.tsx
+++ src/components/article/workTogetherCTA/WorkTogetherCTA.tsx
@@ -8,12 +8,12 @@ export default function WorkTogetherCTA() {
   const t = useTranslations("articlePage.workTogetherCta");
 
   return (
-    <section className="pt-[278px] xl:pt-[267px]">
+    <section className="pt-[278px] xl:pt-[267px] pb-[61px] xl:pb-[292px]">
       <Container>
-        <SectionTitle className="max-w-[283px] xl:max-w-[376px] mb-[284px] md:mb-8 text-[48px] xl:text-[64px]">
+        <SectionTitle className="max-w-[293px] xl:max-w-[386px] mb-[284px] md:mb-8 text-[48px] xl:text-[64px]">
           {t("title")}
         </SectionTitle>
-        <Link href="/vacancies">
+        <Link href="/vacancies" className="block max-w-[328px] mx-auto md:mx-0">
           <MainButton>{t("button")}</MainButton>
         </Link>
       </Container>
