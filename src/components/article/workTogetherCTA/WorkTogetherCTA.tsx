048e3bd69120a96ec96848fc7fb549529987fc24
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-17 15:39:58 +0300

    added cta

--- src/components/article/workTogetherCTA/WorkTogetherCTA.tsx
+++ src/components/article/workTogetherCTA/WorkTogetherCTA.tsx
@@ -1,3 +1,22 @@
+import MainButton from "@/components/shared/buttons/MainButton";
+import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/sectionTitle/SectionTitle";
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
+
 export default function WorkTogetherCTA() {
-  return <section>CTA</section>;
+  const t = useTranslations("articlePage.workTogetherCta");
+
+  return (
+    <section className="pt-[278px] xl:pt-[267px]">
+      <Container>
+        <SectionTitle className="max-w-[283px] xl:max-w-[376px] mb-[284px] md:mb-8 text-[48px] xl:text-[64px]">
+          {t("title")}
+        </SectionTitle>
+        <Link href="/vacancies">
+          <MainButton>{t("button")}</MainButton>
+        </Link>
+      </Container>
+    </section>
+  );
 }
