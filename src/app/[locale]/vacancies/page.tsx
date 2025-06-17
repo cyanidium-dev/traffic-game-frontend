75e290d21416c90f447a03eb078714d1ac7aa0d7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-17 19:37:09 +0300

    added vacancies query

--- src/app/[locale]/vacancies/page.tsx
+++ src/app/[locale]/vacancies/page.tsx
@@ -1,11 +1,25 @@
+import Loader from "@/components/shared/loader/Loader";
 import Hero from "@/components/vacancies/hero/Hero";
 import VacanciesList from "@/components/vacancies/vacanciesList/VacanciesList";
+import { fetchSanityData } from "@/utils/fetchSanityData";
+import { Suspense } from "react";
 
-export default function VacanciesPage() {
+interface VacanciesPageProps {
+  params: Promise<{ locale: Locale }>;
+}
+
+export default async function VacanciesPage({ params }: VacanciesPageProps) {
+  const { locale } = await params;
+  const vacanciesList = await fetchSanityData(allPostsQuery, {
+    lang: locale,
+  });
+
   return (
     <>
       <Hero />
-      <VacanciesList />
+      <Suspense fallback={<Loader />}>
+        <VacanciesList vacanciesList={vacanciesList} />
+      </Suspense>
     </>
   );
 }
