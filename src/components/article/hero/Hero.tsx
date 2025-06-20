f3b131ca6166c65eff00c0f458a34bf820ef41ac
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-20 23:02:55 +0300

    added parallax

--- src/components/article/hero/Hero.tsx
+++ src/components/article/hero/Hero.tsx
@@ -1,25 +1,47 @@
+"use client";
 import Container from "@/components/shared/container/Container";
 import EstimatedReadingTime from "@/components/shared/estReadingTime/EstimatedReadingTime";
 import { Post } from "@/types/post";
 import Image from "next/image";
+import * as motion from "motion/react-client";
+import { useScroll } from "framer-motion";
+import { useRef } from "react";
+import { useParallax } from "@/hooks/useParallax";
 
 interface HeroProps {
   article: Post;
 }
 
 export default function Hero({ article }: HeroProps) {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  // Паралакс-значення
+  const y1 = useParallax(scrollYProgress, 280);
   const { image, title, description } = article;
 
   return (
-    <section className="relative w-full pt-[176px] xl:pt-[171px] pb-21 xl:pb-[78px]">
-      <Image
-        src={image}
-        fill
-        alt="article image"
-        sizes="100vw"
-        priority
-        className="-z-10 object-cover rounded-b-[8px]"
-      />
+    <section
+      ref={ref}
+      className="relative w-full pt-[176px] xl:pt-[171px] pb-21 xl:pb-[78px] overflow-hidden"
+    >
+      <motion.div
+        style={{ y: y1 }}
+        className="absolute -z-10 top-[-35px] left-0 w-full h-full"
+      >
+        <Image
+          src={image}
+          fill
+          alt="article image"
+          sizes="100vw"
+          priority
+          className="-z-10 object-cover rounded-b-[8px]"
+        />
+      </motion.div>
       <Container>
         <h1 className="md:max-w-[427px] mb-5 font-actay text-[36px] xl:text-[40px] font-bold leading-[120%] uppercase">
           {title}
--- src/components/article/workTogetherCTA/CTAImages.tsx
+++ src/components/article/workTogetherCTA/CTAImages.tsx
@@ -1,12 +1,29 @@
+"use client";
 import Image from "next/image";
 import * as motion from "motion/react-client";
+import { useScroll } from "framer-motion";
+import { useRef } from "react";
 import { fadeInAnimation } from "@/utils/animationVariants";
 import TrafficGame from "@/components/shared/trafficGame/TrafficGame";
+import { useParallax } from "@/hooks/useParallax";
 
 export default function CTAImages() {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  // Паралакс-значення
+  const y1 = useParallax(scrollYProgress, 120);
+
   return (
-    <>
-      <div className="md:hidden absolute -z-20 top-[-422px] left-[-291px] w-[407px] h-[369px]">
+    <div ref={ref}>
+      <motion.div
+        style={{ y: y1 }}
+        className="md:hidden absolute -z-20 top-[-422px] left-[-291px] w-[407px] h-[369px]"
+      >
         <Image
           src="/images/article/ellipseMob.svg"
           alt="background"
@@ -14,8 +31,11 @@ export default function CTAImages() {
           sizes="25vw"
           className="object-cover blur-[71px] supports-[backdrop-filter]:blur-[71px] will-change-transform"
         />
-      </div>
-      <div className="hidden md:block absolute -z-20 top-[0px] left-[-306px] w-[480px] h-[436px]">
+      </motion.div>
+      <motion.div
+        style={{ y: y1 }}
+        className="hidden md:block absolute -z-20 top-[0px] left-[-306px] w-[480px] h-[436px]"
+      >
         <Image
           src="/images/article/ellipseDesk.svg"
           alt="background"
@@ -23,7 +43,7 @@ export default function CTAImages() {
           sizes="25vw"
           className="object-cover blur-[84px]  supports-[backdrop-filter]:blur-[84px] will-change-transform"
         />
-      </div>
+      </motion.div>
 
       <div className="md:hidden absolute z-30 bottom-[-231px] left-[calc(50%-658px)] w-[1315px] h-[306px]">
         <Image
@@ -34,7 +54,7 @@ export default function CTAImages() {
           className="object-cover blur-[17px] supports-[backdrop-filter]:blur-[17px] will-change-transform"
         />
       </div>
-      <div className="hidden md:block absolute z-30 bottom-[-415px] xl:bottom-[-555px] left-[calc(50%-1282px)] w-[2564px] h-[329px] xl:h-[529px]">
+      <div className="hidden md:block absolute z-30 bottom-[-415px] xl:bottom-[-555px] left-[calc(50%-1282px)] w-[2564px] h-[329px] xl:h-[409px]">
         <Image
           src="/images/article/ellipseBlackDesk.svg"
           alt="background"
@@ -45,11 +65,7 @@ export default function CTAImages() {
       </div>
 
       <motion.div
-        initial="hidden"
-        whileInView="visible"
-        exit="exit"
-        viewport={{ once: true, amount: 0.1 }}
-        variants={fadeInAnimation({ delay: 0.6, scale: 0.9 })}
+        style={{ y: y1 }}
         className="absolute -z-20 top-[-220px] md:top-[305px] left-[-52px] md:left-[-11px] pointer-events-none"
       >
         <TrafficGame className="max-w-[321px] text-[102px] md:text-[121px] rotate-[-10deg] bg-[linear-gradient(230deg,_#121212_20.29%,_#B00F74_95.97%)]" />
@@ -80,7 +96,12 @@ export default function CTAImages() {
         variants={fadeInAnimation({ delay: 0.6, scale: 0.9 })}
         className="hidden md:block absolute -z-10 top-[-362px] xl:top-[-242px] left-[calc(50%-135px)] w-[811px] h-[1050px] pointer-events-none"
       >
-        <TrafficGame className="absolute -z-20 top-[86px] left-[-21px] max-w-[379px] md:text-[121px] rotate-[30deg] bg-[linear-gradient(96deg,_#121212_3.75%,_#B00F74_95.63%)]" />
+        <motion.div
+          style={{ y: y1 }}
+          className="absolute -z-20 top-[86px] left-[-21px]"
+        >
+          <TrafficGame className=" max-w-[379px] md:text-[121px] rotate-[30deg] bg-[linear-gradient(96deg,_#121212_3.75%,_#B00F74_95.63%)]" />
+        </motion.div>
         <Image
           src="/images/article/ctaImageDesk.webp"
           alt="background"
@@ -89,6 +110,6 @@ export default function CTAImages() {
           className="object-cover"
         />
       </motion.div>
-    </>
+    </div>
   );
 }
